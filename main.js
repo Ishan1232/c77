d86123c744aaad50fdab65b0ea2103e1aa1141f0
Ishan1232 <85748504+Ishan1232@users.noreply.github.com>
2022-04-28 18:30:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("console webpage");
+
+var first_name= "ranbir";
+var second_name= "kapoor";
+
+var full_name= first_name.concat(last_name);
+
+var a= 10;
+var b= 5;
+
+var add= a + b;
+console.log(add);
